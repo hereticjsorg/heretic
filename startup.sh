ee2396994e488cebee5614a3c666fb3fe5dfa45d
Mikhail Matveev <xtreme@rh1.ru>
2023-01-13 11:52:43 +0200

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH=$(
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    pwd -P
+)
+VERSION_FILE="$SCRIPT_PATH/dist/public/heretic/version.json"
+if !(test -f "$VERSION_FILE"); then
+    npm run build
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+fi
+pm2 start
