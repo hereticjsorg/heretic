ddcac9e3afcb1e7125bdc3362a8be31e6e9e9391
Mikhail Matveev <xtreme@rh1.ru>
2023-01-15 23:48:30 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,7 +5,8 @@ SCRIPT_PATH=$(
     pwd -P
 )
 VERSION_FILE="$SCRIPT_PATH/dist/public/heretic/version.json"
-if !(test -f "$VERSION_FILE"); then
+SERVER_FILE="$SCRIPT_PATH/dist/server.js"
+if [!(test -f "$VERSION_FILE");] || [!(test -f "$SERVER_FILE");] then
     npm run build -- --no-color
     if [ $? -ne 0 ]; then
         exit 1
