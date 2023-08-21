d99b109510846405cfb8febca1de12901d30ef26
hereticjsorg <xtreme@rh1.ru>
2023-08-21 18:45:27 +0300

    Avatar editor: work-in-progress

---
+++ src/core/components/havatar/component.js
@@ -0,0 +1,12 @@
+module.exports = class {
+    async onCreate(input) {
+        this.state = {
+            active: false,
+        };
+        if (input.admin) {
+            await import(/* webpackChunkName: "havatar-admin" */ "./style-admin.scss");
+        } else {
+            await import(/* webpackChunkName: "havatar-frontend" */ "./style-frontend.scss");
+        }
+    }
+};
