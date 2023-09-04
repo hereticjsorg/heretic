ed0b9710c66dee08bb0fe4b545f3d7fa43191f30
hereticjsorg <xtreme@rh1.ru>
2023-09-04 14:53:55 +0300

    Mongo-related bugfixes
    New MongoDB river was returning a different data when upsert was set to "true". This has been fixed by processing data in a different format.

--- src/core/defaults/modules/boilerplate/api/apiDataSave.js
+++ src/core/defaults/modules/boilerplate/api/apiDataSave.js
@@ -74,7 +74,7 @@ export default () => ({
                     returnNewDocument: true,
                     upsert: true,
                 });
-                const seq = counterData && counterData.value && counterData.value.seq ? counterData.value.seq : 1;
+                const seq = counterData && counterData.seq ? counterData.seq : 1;
                 const insertResult = await collection.insertOne({
                     id: seq,
                     ...data._default,
