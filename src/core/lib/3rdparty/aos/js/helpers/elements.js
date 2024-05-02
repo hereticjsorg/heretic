d09f0f4c0ae8a7074d3b874fd633a715f8a50b61
hereticjsorg <xtreme@rh1.ru>
2024-05-02 17:00:12 +0300

    AOS library refactored & added

---
+++ src/core/lib/3rdparty/aos/js/helpers/elements.js
@@ -0,0 +1,11 @@
+/**
+ * Generate initial array with elements as objects
+ * This array will be extended later with elements attributes values
+ * like 'position'
+ */
+export default () => {
+    const elements = document.querySelectorAll("[data-aos]");
+    return Array.prototype.map.call(elements, node => ({
+        node
+    }));
+};
