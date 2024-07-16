487c390da144da57a7183023cd64830732d9f589
hereticjsorg <xtreme@rh1.ru>
2024-07-16 14:59:15 +0300

    Loading spinner fixes

--- src/core/components/hloading-dots/component.js
+++ src/core/components/hloading-dots/component.js
@@ -1,9 +1,20 @@
-module.exports = class {
+import {
+    v4 as uuid,
+} from "uuid";
+
+export default class {
     async onCreate(input) {
+        this.state = {
+            cid: uuid(),
+        };
         if (input.admin) {
-            await import(/* webpackChunkName: "hloading-dots-admin" */ "./style-admin.scss");
+            await import( /* webpackChunkName: "hloading-dots-admin" */ "./style-admin.scss");
         } else {
-            await import(/* webpackChunkName: "hloading-dots-frontend" */ "./style-frontend.scss");
+            await import( /* webpackChunkName: "hloading-dots-frontend" */ "./style-frontend.scss");
         }
     }
-};
+
+    onMount() {
+        setTimeout(() => document.getElementById(this.state.cid).style.opacity = "1", 300);
+    }
+}
